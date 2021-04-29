2aee18f11b116acc289cdcc82c1d6c2df867c1dd
Jay Gibble <76593211+punchjay@users.noreply.github.com>
2021-04-29 11:57:53 -0700

    homework02 update

--- homework02/Program.cs
+++ homework02/Program.cs
@@ -8,24 +8,31 @@ namespace homework02
     {
         static void Main(string[] args)
         {
-            var Users = new List<Models.User>
-            {
-                new Models.User { Name = "Dave", Password = "hello" },
-                new Models.User { Name = "Steve", Password = "steve" },
-                new Models.User { Name = "Lisa", Password = "hello" }
-            };
+            var Users = new List<Models.User>();
+
+            Users.Add(new Models.User { Name = "Dave", Password = "hello" });
+            Users.Add(new Models.User { Name = "Steve", Password = "steve" });
+            Users.Add(new Models.User { Name = "Lisa", Password = "hello" });
 
             //Users.ForEach(user => Console.Write($"Name: {user.Name} - Password: {user.Password}\t"));
-
-            //using lambda operator same query as below
-            var QueryPasswordHello = Users.Where(user => user.Password == "hello");
 
             //var QueryPasswordHello =
             //    from user in Users
             //    where user.Password == "hello"
             //    select user;
 
+            //using lambda operator same query as above
+            var QueryPasswordHello = Users.Where(user => user.Password == "hello");
+            Console.WriteLine("Users where the password is 'hello':");
             foreach (var user in QueryPasswordHello)
+            {
+                Console.WriteLine(user.Name);
+            }
+
+            var QuerySameNameAsPassord = Users.Where(user => user.Name == user.Password);
+            //Users.RemoveAll(QuerySameNameAsPassord);
+
+            foreach (var user in QuerySameNameAsPassord)
             {
                 Console.WriteLine(user.Name);
             }
