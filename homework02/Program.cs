a4d4bb39fa0a6e2a59f417281fc2d0f5992bed42
Jay Gibble <76593211+punchjay@users.noreply.github.com>
2021-04-30 14:56:12 -0700

    homework02 cleanup

--- homework02/Program.cs
+++ homework02/Program.cs
@@ -26,13 +26,10 @@ namespace homework02
             //using lambda operator same query as above
             var QueryPasswordHello = Users.Where(user => user.Password.ToUpper() == "HELLO");
             Console.WriteLine("Users where the password is 'hello':");
-            foreach (var user in QueryPasswordHello)
-            {
-                Console.WriteLine(user.Name);
-            }
+            foreach (var user in QueryPasswordHello) Console.WriteLine(user.Name);
             Console.WriteLine("\n");
 
-            var QuerySameNameAsPassword = Users.Where(user => user.Name.ToLower() == user.Password).ToList();
+            var QuerySameNameAsPassword = Users.Where(user => user.Name.ToLower() == user.Password);
             Console.WriteLine("Users where lower-cased password version of their name:");
             foreach (var user in QuerySameNameAsPassword)
             {
