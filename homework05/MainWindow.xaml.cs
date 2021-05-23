ef3c6662545760d0dda76b13f0ffc63b90bc1ad4
Jay Gibble <76593211+punchjay@users.noreply.github.com>
2021-05-23 14:18:35 -0700

    homework cleanup

--- homework05/MainWindow.xaml.cs
+++ homework05/MainWindow.xaml.cs
@@ -9,11 +9,15 @@ namespace homework05
     public partial class MainWindow : Window
     {
         int userTurn;
+        string startTx = "To begin Tic-Tac-Toe, X goes first, then O.";
+        string turnTx = "'s turn.";
+        string resultTx = "is the winner!!!";
+        string drawTx = "Draw, no one";
 
         public MainWindow()
         {
             InitializeComponent();
-            uxTurn.Text = "To begin Tic-Tac-Toe, X goes first, then O.";
+            uxTurn.Text = startTx;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -22,20 +26,20 @@ namespace homework05
             if (userTurn == 1)
             {
                 clickedBtn.Content = "O";
-                uxTurn.Text = "X's turn.";
+                uxTurn.Text = $"X{turnTx}";
             }
             else
             {
                 clickedBtn.Content = "X";
-                uxTurn.Text = "O's turn.";
+                uxTurn.Text = $"O{turnTx}";
             }
             clickedBtn.IsEnabled = false;
-            CheckGameWinner(clickedBtn);
+            CheckGameResult(clickedBtn);
             userTurn += 1;
             if (userTurn > 1) userTurn = 0;
         }
 
-        private void CheckGameWinner(Button btnClicked)
+        private void CheckGameResult(Button btnClicked)
         {
             if ((btn01.Content == btnClicked.Content & btn02.Content == btnClicked.Content & btn03.Content == btnClicked.Content)
                 || (btn01.Content == btnClicked.Content & btn04.Content == btnClicked.Content & btn07.Content == btnClicked.Content)
@@ -46,14 +50,14 @@ namespace homework05
                 || (btn07.Content == btnClicked.Content & btn08.Content == btnClicked.Content & btn09.Content == btnClicked.Content)
                 || (btn03.Content == btnClicked.Content & btn05.Content == btnClicked.Content & btn07.Content == btnClicked.Content))
             {
-                if ((string)btnClicked.Content == "O") uxTurn.Text = "O is the winner!!!";
-                else if ((string)btnClicked.Content == "X") uxTurn.Text = "X is the winner!!!";
+                if ((string)btnClicked.Content == "O") uxTurn.Text = $"O {resultTx}";
+                else if ((string)btnClicked.Content == "X") uxTurn.Text = $"X {resultTx}";
                 foreach (Button btn in uxGrid.Children) btn.IsEnabled = false;
             }
             else
             {
                 foreach (Button btn in uxGrid.Children) if (btn.IsEnabled == true) return;
-                uxTurn.Text = "Draw, no one is the winner!!!";
+                uxTurn.Text = $"{drawTx} {resultTx}";
             }
         }
 
@@ -64,7 +68,7 @@ namespace homework05
                 btn.Content = "";
                 btn.IsEnabled = true;
                 userTurn = 0;
-                uxTurn.Text = "To begin Tic-Tac-Toe, X goes first, then O.";
+                uxTurn.Text = startTx;
             }
         }
 
