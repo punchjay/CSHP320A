c863a65efa888e41b7c13e201205d9a6cdafd36f
Jay Gibble <76593211+punchjay@users.noreply.github.com>
2021-05-26 15:48:15 -0700

    added model

---
+++ WishListApp/WishListApp/Models/WishListModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WishListApp.Models
+{
+    public class WishListModel
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Sku { get; set; }
+        public bool InStock { get; set; }
+        public int Qty { get; set; }
+        public string Notes { get; set; }
+        public System.DateTime CreatedDate { get; set; }
+
+        public WishListRepository.WishListModel ToRepositoryModel()
+        {
+            var repositoryModel = new WishListRepository.WishListModel
+            {
+                Brand = Brand,
+                CreatedDate = CreatedDate,
+                Description = Description,
+                Id = Id,
+                InStock = InStock,
+                Notes = Notes,
+                Price = Price,
+                Qty = Qty,
+                Sku = Sku
+            };
+
+            return repositoryModel;
+        }
+
+        public static WishListModel ToModel(WishListRepository.WishListModel respositoryModel)
+        {
+            var wishListModel = new WishListModel
+            {
+                Brand = respositoryModel.Brand,
+                CreatedDate = respositoryModel.CreatedDate,
+                Description = respositoryModel.Description,
+                Id = respositoryModel.Id,
+                InStock = respositoryModel.InStock,
+                Notes = respositoryModel.Notes,
+                Price = respositoryModel.Price,
+                Qty = respositoryModel.Qty,
+                Sku = respositoryModel.Sku
+            };
+
+            return wishListModel;
+        }
+    }
+}
