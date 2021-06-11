86eb4beea173976f14f03d8b00fbceaa43f7111c
Jay Gibble <76593211+punchjay@users.noreply.github.com>
2021-06-10 21:51:09 -0700

    added MVC, Wiring up

--- homework06/Models/Card.cs
+++ homework06/Models/Card.cs
@@ -1,9 +1,14 @@
-﻿namespace homework06.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace homework06.Models
 {
     public class Card
     {
+        [Required(ErrorMessage = "Please enter your name")]
         public string From { get; set; }
+        //[Required(ErrorMessage = "Please enter your birthday person")]
         public string To { get; set; }
+        //[Required(ErrorMessage = "Please enter your message")]
         public string Message { get; set; }
     }
 }
